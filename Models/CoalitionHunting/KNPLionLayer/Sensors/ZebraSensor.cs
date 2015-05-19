92986d9961d6ccbf6fe0445723e6d7ad0b128aa3
Allquantor <morozovwin@googlemail.com>
2015-05-19 14:19:33 +0200

    added code for ZebraSensor

--- Models/CoalitionHunting/KNPLionLayer/Sensors/ZebraSensor.cs
+++ Models/CoalitionHunting/KNPLionLayer/Sensors/ZebraSensor.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DalskiAgent.Perception;
+using SpatialAPI.Environment;
 
-namespace KNPLionLayer
+
+namespace KNPLionLayer.Sensors
 {
-	public class ZebraSensor
+	class ZebraSensor : ISensor
 	{
-		public ZebraSensor ()
+		private readonly IEnvironment environment;
+		public ZebraSensor(IEnvironment env)
 		{
+			environment = env;
+		}
+
+		object Sense()
+		{
+			IEnumerable<Zebra> elephants =  environment.ExploreAll().OfType<Zebra>();
+			return elephants;
+
+			// Einschränkung über die Postion des gesehenen Elefanten und der eigenen Position.
 		}
 	}
 }
-
