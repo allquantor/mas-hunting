6720aad5c96faea6d35366c1b8e8b6f3871d7815
flbaue <florian.bauer@posteo.de>
2015-05-19 14:54:11 +0200

    added environment layer

---
+++ Models/CoalitionHunting/KNPEnvironmentLayer/IKNPEnvironmentLayer.cs
@@ -0,0 +1,10 @@
+﻿using Hik.Communication.ScsServices.Service;
+using LifeAPI.Layer;
+using SpatialAPI.Environment;
+
+namespace KNPEnvironmentLayer
+{
+    [ScsService(Version = "0.1")]
+    public interface IKNPEnvironmentLayer : ISteppedActiveLayer, IEnvironment
+    {}
+}
