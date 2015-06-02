19c6b7d991925bdd69f7193fc47753a9355e6843
Allquantor <morozovwin@googlemail.com>
2015-06-02 10:23:51 +0200

    last status

--- Models/CoalitionHunting/KNPLionLayer/KNPZebraLayer/Agents/Zebra.cs
+++ Models/CoalitionHunting/KNPLionLayer/KNPZebraLayer/Agents/Zebra.cs
@@ -3,24 +3,78 @@ using DalskiAgent.Agents;
 using LifeAPI.Layer;
 using SpatialAPI.Environment;
 using SpatialAPI.Shape;
+using SpatialAPI.Entities.Transformation;
+using SpatialAPI.Entities.Movement;
 
 namespace KNPZebraLayer
 {
 	public class Zebra :SpatialAgent,IZebra
 	{
-		public Zebra (ILayer layer,
-		RegisterAgent registerAgent,
-		UnregisterAgent unregisterAgent,
-		IEnvironment environment,
-		Guid id,
-		IShape shape,
-		double lat,
-		double lon,
-		double imageCoordX,
-		double imageCoordY,
-		)
-		{
+
+
+
+		private readonly double _lat;
+		private readonly double _lon;
+		private readonly double _imageCoordX;
+		private readonly double _imageCoordY;
+		private string state;
+		private Direction preyDirection;
+		private double preySpeed;
+		private double maxSPeed;
+		private string strategy;
+
+
+
+
+		public Zebra 
+		(ILayer layer,
+			RegisterAgent registerAgent,
+			UnregisterAgent unregisterAgent,
+			IEnvironment environment,
+			Guid id,
+			IShape shape,
+			double lat,
+			double lon,
+			double imageCoordX,
+			double imageCoordY)
+			:
+		base(layer, registerAgent, unregisterAgent, environment, id, shape, collisionType:CollisionType.Ghost) {
+			_lat = lat;
+			_lon = lon;
+			_imageCoordX = imageCoordX;
+			_imageCoordY = imageCoordY;
+			state = "chill";
+
+			SensorArray.AddSensor(new LionSensor(environment));
 		}
+
+		#region IZebra implementation
+
+		public Vector3 getPosition ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public Direction GetDirection ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public int getStatus ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion
+
+		#region IAgentLogic implementation
+
+		public DalskiAgent.Interactions.IInteraction Reason ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion
 	}
 }
 
